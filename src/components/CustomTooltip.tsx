531fc2763f3f6853ca010052fdce7b3f034dc2aa
Guilherme Pereira <guilhermepereira2917@gmail.com>
2025-03-22 15:04:27 -0300

    add custom cursor

--- src/components/CustomTooltip.tsx
+++ src/components/CustomTooltip.tsx
@@ -8,12 +8,14 @@ interface CustomTooltipProps {
 
 export default function CustomTooltip({ anchorSelect, title, description }: CustomTooltipProps) {
   return (
-    <Tooltip anchorSelect={anchorSelect} place="bottom-start" float={true} opacity={1} noArrow offset={20} style={{
-      backgroundColor: "var(--color-light-beige)",
-      borderRadius: 20,
-      zIndex: 1000,
-      transition: "none",
-    }}>
+    <Tooltip anchorSelect={anchorSelect} place="bottom-start" float={true} opacity={1} offset={25} noArrow
+      style={{
+        backgroundColor: "var(--color-light-beige)",
+        borderRadius: 20,
+        zIndex: 1000,
+        transition: "none",
+      }}
+    >
       <p>
         <span className="text-dark-beige-alternative">{title}</span> <br />
         <span className="text-medium-beige">{description}</span>
